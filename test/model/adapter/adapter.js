412d3762955846019ad5f7e1d981021287f29f48
Vladislav <rabbiabram@gmail.com>
2014-12-11 00:59:17 +0700

    Delete adapter.js

--- test/model/adapter/adapter.js
+++
@@ -1,9 +0,0 @@
-var Adapter = require('../../../src/model/adapter/Adapter.js');
-var except = require('chai').expect;
-describe('Adapter', function() {
-	var ad = new Adapter();
-	it('should have client property', function() {
-		// assert.property(ad, 'client')
-		except(ad).to.have.ownProperty('client');
-	});
-});
