3ad90252e0335a1d62ed7b0d869b88fe63b700f1
Vladislav Bezverkhiy <bezverhiy@a2design.biz>
2014-12-31 23:32:34 +0700

    Updated streams tests

--- test/streams/CustomStreamTest.js
+++ test/streams/CustomStreamTest.js
@@ -8,7 +8,11 @@ var BaseStream = require('../../lib/streams/BaseStream.js');
 
 describe('CustomStream', function() {
 	var r_stream = new CustomStream();
+    var r_stream2 = CustomStream();
 	it('should be a instance of BaseStream', function() {
 		r_stream.should.be.an.instanceof(BaseStream);
+        r_stream2.should.be.an.instanceof(BaseStream);
+        r_stream.should.be.an.instanceof(CustomStream);
+        r_stream2.should.be.an.instanceof(CustomStream);
 	});
 });
--- test/streams/HttpStreamTest.js
+++ test/streams/HttpStreamTest.js
@@ -10,7 +10,9 @@ var HttpStream = require('../../lib/Lib.js').HttpStream;
 describe('HttpStream', function() {
 	var httpStream = HttpStream();
 	it('should be an instance of BaseStream', function() {
+		var anotherStream = new HttpStream();
 		httpStream.should.be.an.instanceof(BaseStream);
+		anotherStream.should.be.an.instanceof(BaseStream);
 	});
 	it('should have a property #http', function() {
 		httpStream.should.have.a.property('http');
@@ -24,14 +26,40 @@ describe('HttpStream', function() {
 		httpStream.start.should.be.a('function');
 	});
 	describe('.listen()', function() {
+		var stream = new HttpStream();
 		it('should return same object', function() {
-			httpStream.listen(3001).should.be.an.instanceof(HttpStream);
+			stream.listen().should.be.an.instanceof(HttpStream);
 		});
-		it('should start http server', function() {
-			httpStream.http.close(function() {
-				httpStream.http.should.be.not.equal(null);
+		it('should start http server', function(done) {
+			stream.http.should.be.not.equal(null);
+			stream.listen();
+			stream.http.close(function() {
+				done();
+				// an_stream.listen();
+				// an_stream.http.close(function() {
+				// 	// loc_stream.listen(3002, '127.0.0.1');
+				// 	// loc_stream.http.close(function() {
+				// 	// 	done();			
+				// 	// });
+				// })
 			});
 		});
+		it('should start http server on specifed port', function(done) {
+			var stream = new HttpStream();
+			stream.listen(3004);
+			stream.port.should.be.equal(3004);
+			stream.http.close(function() {
+				done();
+			});
+		});
+		// it('should start http server on specifed host', function(done) {
+		// 	var stream = new HttpStream();
+		// 	stream.listen(3004, '192.168.1.1');
+		// 	stream.port.should.be.equal(3004);
+		// 	stream.http.close(function() {
+		// 		done();
+		// 	})
+		// });
 	});
 	describe('.end()', function() {
 		var stream = new HttpStream();
@@ -41,11 +69,16 @@ describe('HttpStream', function() {
 		after(function() {
 			delete stream;
 		});
-		it('should stop http server and set #http to null', function() {
-			stream.end();
-			stream.onEnd(function() {
-				assert.equal(stream.http, null);
+		it('should stop http server and set #http to null', function(done) {
+			var an_stream = new HttpStream();
+			an_stream.onEnd(function() {
+				done();
 			});
+			stream.onEnd(function() {
+				an_stream.listen(3002);
+				an_stream.end();
+			});
+			stream.end();
 		});
 	});
 	describe('.start()', function() {
