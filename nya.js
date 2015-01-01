80fd973123760a6a09a9529b55ccc660c188359c
Vladislav Bezverkhiy <bezverhiy@a2design.biz>
2015-01-01 23:08:28 +0700

    Updated nya.js

--- nya.js
+++ nya.js
@@ -4,5 +4,10 @@ var NYA = function() {
 
 NYA.prototype.lib = require('./lib/Lib.js');
 NYA.prototype.RequestStream = NYA.prototype.lib.RequestStream;
+NYA.prototype.HttpStream = NYA.prototype.lib.HttpStream;
+NYA.prototype.ResponseStream = NYA.prototype.lib.ResponseStream;
+NYA.prototype.RouteStream = NYA.prototype.lib.RouteStream;
+NYA.prototype.CustomStream = NYA.prototype.lib.CustomStream;
+
 
 module.exports = new NYA();
